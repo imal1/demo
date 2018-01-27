46cee1152d28d224b83e87ed9cb6aac8e68a13b8
imali <chjczj@126.com>
2018-01-27 09:48:54 +0800

    Add files via upload

---
+++ src/components/navigator.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+      <wxc-minibar title=""
+                   background-color="#374361"
+                   text-color="#e53935"
+                   leftButton="">
+                   <text class="title" style="font-size:35px; color:white;" slot="middle">订 仓</text>
+      </wxc-minibar>
+	</div>
+</template>
+<style scoped>
+	.title{
+		margin-left: -500px;
+	}
+</style>
+<script>
+	import { WxcMinibar } from 'weex-ui';
+	export default {
+  	components:{WxcMinibar}
+  };
+</script>
---
+++ src/components/searchbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <wxc-searchbar class="searchbar"
+      							 ref="wxc-searchbar"
+                     @wxcSearchbarCancelClicked="wxcSearchbarCancelClicked"
+                     @wxcSearchbarInputReturned="wxcSearchbarInputReturned"
+                     @wxcSearchbarInputOnInput="wxcSearchbarInputOnInput"
+                     @wxcSearchbarCloseClicked="wxcSearchbarCloseClicked"
+                     @wxcSearchbarInputOnFocus="wxcSearchbarInputOnFocus"
+                     @wxcSearchbarInputOnBlur="wxcSearchbarInputOnBlur"></wxc-searchbar>
+      <text class="value-text">{{value}}</text>
+  </div>
+</template>
+<script>
+  import { WxcSearchbar } from 'weex-ui'
+  export default {
+    components: { WxcSearchbar },
+    data: () => ({
+      value: ''
+    }),
+    methods: {
+      wxcSearchbarInputOnFocus () {
+      },
+      wxcSearchbarInputOnBlur () {
+      },
+      wxcSearchbarInputReturned (){
+      },
+      wxcSearchbarCloseClicked () {
+      },
+      /*wxcSearchbarInputOnInput (e) {
+        this.value = e.value;
+      },*/
+      wxcSearchbarCancelClicked () {
+      },
+      wxcSearchbarInputDisabledClicked () {
+      },
+      wxcSearchbarDepChooseClicked () {
+      }
+    }
+  };
+</script>
